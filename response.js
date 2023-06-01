bf503212e4d76e76d1248668a0cf589e31044e49
nonezzc <41660854+nonezzc@users.noreply.github.com>
2023-06-01 20:38:01 +0800

    Update response.js

--- response.js
+++ response.js
@@ -8,7 +8,7 @@
 // @include    https://*.tiktokglobalshop.com/*
 // @include    https://affiliate.tiktok.com/*
 // @include    *://localhost*
-// @include    *://b4fe-183-11-38-248.ngrok-free.app*
+// @include    *://89ad-113-102-167-30.ngrok-free.app*
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_addStyle
@@ -25,7 +25,7 @@
 // @require https://greasyfork.org/scripts/434540-layerjs-gm-with-css/code/layerjs-gm-with-css.js?version=1065982
 // @connect     localhost
 // @connect     raw.githubusercontent.com
-// @connect     b4fe-183-11-38-248.ngrok-free.app
+// @connect     89ad-113-102-167-30.ngrok-free.app
 // @antifeature  payment
 // @license Creative Commons
 // ==/UserScript==
